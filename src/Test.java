abf4f87e7b13e339454745daea70fed4523ad9c2
Jasin Ibishi <jasinibishi@gmail.com>
2023-12-22 09:44:25 +0100

    first commit

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+//A Java class to test the encapsulated class.   
+class Test{  
+public static void main(String[] args){  
+//creating instance of the encapsulated class  
+Student s=new Student();  
+//setting value in the name member  
+s.setName("Jasin");  
+//getting value of the name member  
+System.out.println(s.getName());  
+}  
+}  
